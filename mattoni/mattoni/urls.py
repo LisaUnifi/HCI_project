9dc115d535ef5af4b5df07dff810ce3b8b698231
Lisa Cresti <lisa@AsusL12>
2022-01-04 12:21:38 +0100

    start from: https://docs.djangoproject.com/en/3.0/intro/tutorial02/ - Playing with the API

--- mattoni/mattoni/urls.py
+++ mattoni/mattoni/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    #path include nella prima parte il nome della pagina e nella seconda il contenuto 
+    path('', include('scheda.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ mattoni/scheda/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SchedaConfig(AppConfig):
+    name = 'scheda'
