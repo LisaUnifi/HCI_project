62830ab5958bf65b6933968b976841c5bcfee051
Lisa Cresti <lisa@AsusL12>
2022-01-06 11:14:00 +0100

    Conclusione Into Django

--- mattoni/scheda/models.py
+++ mattoni/scheda/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+import datetime
+from django.utils import timezone 
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     example = models.IntegerField(default = 7)
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
