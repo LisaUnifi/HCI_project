95b588f1a285995a40363ab5a9220018fbcb14d3
Lisa Cresti <lisa@AsusL12>
2022-01-17 19:44:01 +0100

    Update

--- mattoni/scheda/models.py
+++ mattoni/scheda/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.utils import timezone 
 
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -17,3 +18,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
