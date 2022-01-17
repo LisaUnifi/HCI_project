fa49aaa1a0b72b546b30cbd722041eb0ab80a453
Lisa Cresti <lisa@AsusL12>
2022-01-17 16:15:53 +0100

    Update

--- mattoni/mattoni/urls.py
+++ mattoni/mattoni/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView 
 
 urlpatterns = [
     #path include nella prima parte il nome della pagina e nella seconda il contenuto 
-    path('', include('scheda.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('scheda/', include('scheda.urls')),
     path('admin/', admin.site.urls),
+    
 ]
