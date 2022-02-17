c131243ddaac4f0e7466faf4e46aba34e703d41a
Lisa Cresti <lisa@AsusL12>
2022-02-17 11:43:00 +0100

    Implementazione scheda

--- mattoni/mattoni/settings.py
+++ mattoni/mattoni/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n0+qch)kjti*g=7cw*hj^s(nf6zm+%@(ju_4ce_3hzzzhtmm6i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.126','0.0.0.0']
+ALLOWED_HOSTS = ['192.168.1.126','0.0.0.0', '192.168.1.20']
 
 
 # Application definition
